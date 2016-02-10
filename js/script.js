30fa53804d7e69d9191c3ebd60495947e4405e28
lurai <admin@lurailabs.com>
2016-02-10 14:15:48 +0100

    Now one click in RUN executes HTML, CSS and JS

--- js/script.js
+++ js/script.js
@@ -3,36 +3,43 @@
  */
 
 
+function Display() {
 
+    var frame = $('#display').get(0);
 
-// HTML
+    this.update = function() {
+        updateHtml();
+        updateCss();
+        updateJs();
+    };
 
-function updateHtml() {
+    var updateHtml = function() {
+        var frameDoc            = frame.contentDocument || frame.contentWindow.document;
+        frameDoc.body.innerHTML = document.getElementById("htmlCode").value;
+    };
 
-    var frame               = $('#display').get(0);
-    var frameDoc            = frame.contentDocument || frame.contentWindow.document;
-    frameDoc.body.innerHTML = document.getElementById("htmlCode").value;
+    var updateCss = function() {
+        var style           = frame.contentWindow.document.createElement('style');
+        style.innerHTML     = document.getElementById("cssCode").value;
+        frame.contentWindow.document.head.appendChild(style);
+    };
+
+    var updateJs = function() {
+        var script          = frame.contentWindow.document.createElement('script');
+        script.innerHTML    = document.getElementById("jsCode").value;
+        frame.contentWindow.document.body.appendChild(script);
+    };
 }
 
+(function() {
 
-// CSS
+    var display     = new Display();
+    var runButton   = $('#run');
 
-function updateCss() {
+    runButton.click(function update() {
+        display.update();
+    });
 
-    var frame           = $('#display').get(0);
-    var style           = frame.contentWindow.document.createElement('style');
-    style.innerHTML     = document.getElementById("cssCode").value;
-    frame.contentWindow.document.head.appendChild(style);
+}());
 
-}
-
-// JavaScript
-
-function updateJs() {
-
-    var frame           = $('#display').get(0);
-    var script          = frame.contentWindow.document.createElement('script');
-    script.innerHTML    = document.getElementById("jsCode").value;
-    frame.contentWindow.document.body.appendChild(script);
-}
 
