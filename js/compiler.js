7a978a1e8317aba6315d69ea4be7168497add74a
lurai <admin@lurailabs.com>
2016-02-13 21:21:00 +0100

    Refactoring + changes in header + code navbar buttons

--- js/compiler.js
+++ js/compiler.js
@@ -2,33 +2,45 @@
  * Created by lurai on 12/02/2016.
  */
 
-function Display() {
+/**
+ * Takes the input from the HTML, CSS and JS textareas
+ * and executes the code in the iframe.
+ *
+ * @returns {{run: run}}
+ * @constructor
+ */
+
+function Compiler() {
 
     var frame = $('#output').get(0);
 
-    this.update = function () {
-        updateHtml();
-        updateCss();
-        updateJs();
-    };
-
-    var updateHtml = function () {
+    var runHtml = function () {
         var htmlCode            = $("#htmlCode > textarea").val();
         var frameDoc            = frame.contentDocument || frame.contentWindow.document;
         frameDoc.body.innerHTML = htmlCode;
     };
 
-    var updateCss = function () {
+    var runCss = function () {
         var cssCode     = $("#cssCode > textarea").val();
         var style       = frame.contentWindow.document.createElement('style');
         style.innerHTML = cssCode;
         frame.contentWindow.document.head.appendChild(style);
     };
 
-    var updateJs = function () {
+    var runJs = function () {
         var jsCode          = $("#jsCode > textarea").val();
         var script          = frame.contentWindow.document.createElement('script');
         script.innerHTML    = jsCode;
         frame.contentWindow.document.body.appendChild(script);
+    };
+
+    var run = function () {
+        runHtml();
+        runCss();
+        runJs();
+    };
+
+    return {
+        run: run
     };
 }
--- js/script.js
+++ js/script.js
@@ -4,10 +4,38 @@
 
 (function () {
 
-    var display = new Display();
+    /** Create Layout **/
+
+    $('body').layout({
+        north__size:                    65
+        , north__enableCursorHotkey:    false
+        , north__closable:              false
+        , north__resizable:             false
+        , north__spacing_open:          0
+        , north__spacing_close:         0
+        , west__size:                   400
+        , west__spacing_closed:         20
+        , west__togglerLength_closed:   100
+        , west__togglerAlign_closed:    "center"
+        , west__togglerContent_closed:  "S<BR>H<BR>O<BR>W"
+        , west__togglerTip_closed:      "show code"
+        , west__sliderTip:              "slide open code"
+        , west__slideTrigger_open:      "mouseover"
+        , center__maskContents: true // IMPORTANT - enable iframe masking
+    });
+
+    var compiler     = new Compiler();
+    var codePanel   = new CodePanel();
+
+    /** Events **/
 
     $('#runBtn').click(function () {
-        display.update();
+        compiler.run();
+    });
+
+    $('.code-navbar-btn').click(function(event) {
+        codePanel.processBtn(event.target);
+
     });
 
 }());
